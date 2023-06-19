5bb9a4ca4a3be5f2f072b5aafbcac6feca139cdb
NVG-064 <aricandra064@student.esaunggul.ac.id>
2023-06-19 17:28:17 +0700

    Initial rework Home fragment

--- app/src/main/java/com/infinity/socialify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/infinity/socialify/ui/home/HomeFragment.kt
@@ -16,7 +16,7 @@ import com.infinity.socialify.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
   private var _binding: FragmentHomeBinding? = null
-  private var leaderboardCommunityData = ArrayList<LeaderboardCommunity>()
+//  private var leaderboardCommunityData = ArrayList<LeaderboardCommunity>()
 
   // This property is only valid between onCreateView and
   // onDestroyView.
@@ -34,57 +34,57 @@ class HomeFragment : Fragment() {
     return binding.root
   }
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
+//  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//    super.onViewCreated(view, savedInstanceState)
+//
+//    /**
+//     * To binding, id must be camelCase
+//     * Moved to below section
+//     */
+//    binding.rvPapanPeringkat.setHasFixedSize(true)
+//
+//    // Add all data to onCreate() and show the RecyclerView
+//    leaderboardCommunityData.addAll(leaderboardCommunityList)
+//    showRecyclerView()
+//  }
 
-    /**
-     * To binding, id must be camelCase
-     * Moved to below section
-     */
-    binding.rvPapanPeringkat.setHasFixedSize(true)
+//  private val leaderboardCommunityList: ArrayList<LeaderboardCommunity>
+//    get() {
+//      val dataPosition = resources.obtainTypedArray(R.array.data_community_leaderboard_position)
+//      val dataImage = resources.obtainTypedArray(R.array.data_community_leaderboard_picture)
+//      val dataTitle = resources.getStringArray(R.array.data_community_leaderboard_title)
+//      val dataMember = resources.getStringArray(R.array.data_community_leaderboard_total_members)
+//      val dataRating = resources.getStringArray(R.array.data_community_leaderboard_rating)
+//      val dataCategory = resources.getStringArray(R.array.data_community_leaderboard_category)
+//
+//      val dataList = ArrayList<LeaderboardCommunity>()
+//
+//      for (i in dataTitle.indices) {
+//        val leaderboardCom = LeaderboardCommunity(
+//          dataPosition.getResourceId(i, -1),
+//          dataImage.getResourceId(i, -1),
+//          dataTitle[i],
+//          dataMember[i],
+//          dataRating[i],
+//          dataCategory[i]
+//        )
+//
+//        /**
+//         * Check data
+//         */
+//        if (leaderboardCommunityData.size < 3) dataList.add(leaderboardCom)
+//      }
+//
+//      return dataList
+//    }
 
-    // Add all data to onCreate() and show the RecyclerView
-    leaderboardCommunityData.addAll(leaderboardCommunityList)
-    showRecyclerView()
-  }
-
-  private val leaderboardCommunityList: ArrayList<LeaderboardCommunity>
-    get() {
-      val dataPosition = resources.obtainTypedArray(R.array.data_community_leaderboard_position)
-      val dataImage = resources.obtainTypedArray(R.array.data_community_leaderboard_picture)
-      val dataTitle = resources.getStringArray(R.array.data_community_leaderboard_title)
-      val dataMember = resources.getStringArray(R.array.data_community_leaderboard_total_members)
-      val dataRating = resources.getStringArray(R.array.data_community_leaderboard_rating)
-      val dataCategory = resources.getStringArray(R.array.data_community_leaderboard_category)
-
-      val dataList = ArrayList<LeaderboardCommunity>()
-
-      for (i in dataTitle.indices) {
-        val leaderboardCom = LeaderboardCommunity(
-          dataPosition.getResourceId(i, -1),
-          dataImage.getResourceId(i, -1),
-          dataTitle[i],
-          dataMember[i],
-          dataRating[i],
-          dataCategory[i]
-        )
-
-        /**
-         * Check data
-         */
-        if (leaderboardCommunityData.size < 3) dataList.add(leaderboardCom)
-      }
-
-      return dataList
-    }
-
-  private fun showRecyclerView() {
-    binding.rvPapanPeringkat.apply {
-      layoutManager = LinearLayoutManager(requireContext())
-      adapter = LeaderboardCommunityListAdapter(leaderboardCommunityData)
-//      setHasFixedSize(true)
-    }
-  }
+//  private fun showRecyclerView() {
+//    binding.rvPapanPeringkat.apply {
+//      layoutManager = LinearLayoutManager(requireContext())
+//      adapter = LeaderboardCommunityListAdapter(leaderboardCommunityData)
+////      setHasFixedSize(true)
+//    }
+//  }
 
   override fun onDestroyView() {
     super.onDestroyView()
