c40a043ce95318aa6ef8e077ba2339a6e801a27d
NVG-064 <aricandra064@student.esaunggul.ac.id>
2023-06-19 18:09:02 +0700

    Created Spinner with items

--- app/src/main/java/com/infinity/socialify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/infinity/socialify/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -34,6 +36,24 @@ class HomeFragment : Fragment() {
     return binding.root
   }
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    val spinner: Spinner = binding.sortByHomeSpinner
+
+    // Create an ArrayAdapter using the string array and a default spinner layout
+    ArrayAdapter.createFromResource(
+      this.requireContext(),
+      R.array.sort_by_array,
+      android.R.layout.simple_spinner_item
+    ).also { adapter ->
+      // Specify the layout to use when the list of choices appears
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+      // Apply the adapter to the spinner
+      spinner.adapter = adapter
+    }
+  }
 //  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 //    super.onViewCreated(view, savedInstanceState)
 //
