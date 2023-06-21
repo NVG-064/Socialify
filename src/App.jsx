5eb22a7177b9f014c30bcb7b96eae238bba28075
EvaAriani <eva.200180025@mhs.unimal.ac.id>
2023-06-22 00:18:59 +0700

    nambah route halaman

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import Login from "./components/login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import ForgotPassword from "./pages/forgot-password";
+import Diskusi from "./pages/Komunitas-diskusi";
+import Anggota from "./pages/Komunitas-anggota";
+import ModeratordanAdmin from "./pages/Kom-moderator";
+import ForumDiskusi from "./pages/forum-diskusi";
+import ProfileTentang from "./pages/profile-tentang";
+import ProfileKomunitas from "./pages/profile-komunitas";
+import ProfileLinimasa from "./pages/profile-linimasa";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/diskusi" element={<Diskusi />} />
+        <Route path="/komunitas-anggota" element={<Anggota />} />
+        <Route path="/forum-diskusi" element={<ForumDiskusi />} />
+        <Route path="/profil-tentang" element={<ProfileTentang />} />
+        <Route path="/profil-linimasa" element={<ProfileLinimasa />} />
+        <Route path="/profil-komunitas" element={<ProfileKomunitas />} />
+        <Route
+          path="/komunitas-moderator-admin"
+          element={<ModeratordanAdmin />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
