c4c49244f3fad1585d6a720a3323e58f42926c77
Johan Jeques <87894637+Johanjj@users.noreply.github.com>
2023-06-22 08:41:47 +0700

    Update project from Anjayani

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
 import AboutPage from "./pages/AboutPage"
 import SearchCommunityPage from "./pages/SearchCommunityPage"
+import Live0 from "./pages/LiveAudioPage0"
+import Live1 from "./pages/LiveAudioPage1"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" Component={LandingPage} />
         <Route path="/about" Component={AboutPage} />
         <Route path="/search-community" Component={SearchCommunityPage} />
+        <Route path="/live-audio-0" Component={Live0} />
+        <Route path="/live-audio-1" Component={Live1} />
       </Routes>
     </>
   )
