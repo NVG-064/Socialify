837041dee5f3cbf83e09caec2f37b36a26be9c32
NVG-064 <aricandra064@student.esaunggul.ac.id>
2023-06-24 02:05:01 +0700

    Fix NullPointerException and redesigned fragments

--- app/src/main/java/com/infinity/socialify/CommunityFragment.kt
+++ app/src/main/java/com/infinity/socialify/CommunityFragment.kt
@@ -8,67 +8,35 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.infinity.socialify.databinding.FragmentCommunityBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CommunityFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CommunityFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    private val _binding: FragmentCommunityBinding ?= null
+  private var _binding: FragmentCommunityBinding ?= null
 
-    private val binding get() = _binding!!
+  private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentCommunityBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    val navController = findNavController()
+
+    binding.communityPostBackButtonToolbarImageView.setOnClickListener {
+      navController.navigateUp()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val binding = FragmentCommunityBinding.inflate(inflater, container, false)
-        return binding.root
+    binding.communityPostLinearLayout.setOnClickListener {
+      navController.navigate(R.id.navigation_community_menu)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.goToCommunityButton.setOnClickListener {
-            val navController = findNavController()
-            navController.navigate(R.id.navigation_community_menu)
-        }
+    binding.communityPostSettingsButtonImageView.setOnClickListener {
+      navController.navigate(R.id.navigation_settings)
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CommunityFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CommunityFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+  }
 }
